0d6d3f0b4318937fa1997897299d842e57cb1973
Ankita Rajendra Alshi <ankita@140-182-206-185.dhcp-bl.indiana.edu>
2018-10-27 11:36:26 -0400

    initial project

---
+++ planitservices/src/main/java/com/planit/eventplanningservices/EventPlanningServicesApplication.java
@@ -0,0 +1,12 @@
+package com.planit.eventplanningservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPlanningServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventPlanningServicesApplication.class, args);
+	}
+}
